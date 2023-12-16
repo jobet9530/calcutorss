843669618d9d5a2e9eefb071001a61209089e3a6
Jobet Casquejo <87087925+jobet1995@users.noreply.github.com>
2023-12-16 12:11:27 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,103 @@
+import React, { useState } from "react";
 import "./styles.css";
 
-export default function App() {
+const App = () => {
+  const [text, setText] = useState("");
+  const [result, setResult] = useState("");
+  const [isScientific, setIsScientific] = useState(false);
+
+  const customButtonPress = (value) => {
+    if (value === "C") {
+      setText("");
+      setResult("");
+    } else if (value === "=") {
+      try {
+        const evaluatedResult = evaluateExpression(text);
+        setResult(evaluatedResult.toString());
+      } catch (error) {
+        setResult("Error");
+      }
+    } else if (value === "⌫") {
+      if (text.length > 0) {
+        setText(text.substring(0, text.length - 1));
+      }
+    } else if (value === "+/-") {
+      if (text.length > 0 && !text.startsWith("-")) {
+        setText("-" + text);
+      } else if (text.length > 0 && text.startsWith("-")) {
+        setText(text.substring(1));
+      }
+    } else if (value === "%") {
+      try {
+        const percentageResult = (evaluateExpression(text) / 100).toString();
+        setResult(percentageResult);
+      } catch (error) {
+        setResult("Error");
+      }
+    } else {
+      setText(text + value);
+    }
+  };
+
+  const evaluateExpression = (expression) => {
+    try {
+      const result = eval(expression);
+      if (isNaN(result)) {
+        throw new Error("Invalid expression");
+      }
+      return result;
+    } catch (error) {
+      console.error("Error evaluating expression:", error);
+      throw new Error("Invalid expression");
+    }
+  };
+
+  const renderButtons = () => {
+    const rows = [
+      ["C", "7", "8", "9", "/"],
+      ["⌫", "4", "5", "6", "x"],
+      ["+/-", "1", "2", "3", "+"],
+      ["%", "0", ".", "="],
+    ];
+
+    if (isScientific) {
+      rows.push(["sin", "cos", "tan", "sqrt"]);
+    }
+
+    return rows.map((row, rowIndex) => (
+      <div key={rowIndex} className="buttonRow">
+        {row.map((value) => (
+          <button
+            key={value}
+            className="button"
+            onClick={() => customButtonPress(value)}
+          >
+            {value}
+          </button>
+        ))}
+      </div>
+    ));
+  };
+
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="container">
+      <div className="resultContainer">
+        <p className="resultText">{result === "" ? text : result}</p>
+      </div>
+      <div className="buttonContainer">{renderButtons()}</div>
+      <div className="switchContainer">
+        <label>
+          Regular
+          <input
+            type="checkbox"
+            checked={isScientific}
+            onChange={(e) => setIsScientific(e.target.checked)}
+          />
+          Scientific
+        </label>
+      </div>
     </div>
   );
-}
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from 'react';
+import { createRoot } from 'react-dom';
+import './styles.css';
+import App from './App';
 
-import App from "./App";
-
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(
-  <StrictMode>
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>
 );
